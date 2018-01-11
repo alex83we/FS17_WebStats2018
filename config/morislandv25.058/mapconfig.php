6c01067e7c4bd49ca0d900cb1b2848764f0bb610
Alexander Guthmann <aguthmann83@gmail.com>
2018-01-11 13:02:08 +0100

    The Moris Land Konfiguration

--- config/morislandv25.058/mapconfig.php
+++ config/morislandv25.058/mapconfig.php
@@ -1,10 +1,37 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Unbenanntes Dokument</title>
-</head>
+<?php
+/**
+ *
+ * This file is part of the "FS17 Webstats" package.
+ * Copyright (C) 2017-2018 John Hawk <john.hawk@gmx.net>
+ *
+ * "FS17 Webstats" is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * "FS17 Webstats" is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Mapconfig The Moris Land by alex83we 
+ * 
+ */
+$mapVersion = 'The Moris Land v3';
 
-<body>
-</body>
-</html>
+if (empty($mapconfig) || ! is_array($mapconfig)) {
+    $mapconfig = array();
+}
+
+// Farmsilo, Ställe, BGA und TipTrigger
+$mapconfig = array_merge ( $mapconfig, array ( 
+
+) );
+
+// FabrikScripte
+$mapconfig = array_merge ( $mapconfig, array ( 
+
+) );
