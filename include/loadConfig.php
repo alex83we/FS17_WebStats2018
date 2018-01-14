555968e0c8c5528be0b24e3590ad7f8f306ee2ea
Alexander Guthmann <aguthmann83@gmail.com>
2018-01-14 12:43:15 +0100

    Ersteller der Kartenkonfiguration in Infofenster

--- include/loadConfig.php
+++ include/loadConfig.php
@@ -18,44 +18,44 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
-if (! defined ( 'IN_NFMWS' )) {
+if (! defined ('IN_NFMWS')) {
     exit ();
 }
 // Serverkonfiguration laden - wenn nicht vorhanden Instalation starten
 $configFile = './config/server.conf';
-if (file_exists ( $configFile )) {
-	$server = file ( $configFile );
-	$serverConfig = unserialize ( $server [0] );
-	list ( $dSrvIp, $dSrvPort, $dSrvCode, $savegame, $isDediServer, $mapPath ) = $serverConfig;
+if (file_exists($configFile)) {
+	$server = file ($configFile);
+	$serverConfig = unserialize($server[0]);
+	list ($dSrvIp, $dSrvPort, $dSrvCode, $savegame, $isDediServer, $mapPath) = $serverConfig;
 	if ($mapPath == '') {
 		$mapPath = 'nfmarsch29';
 	}
-	$smarty->assign ( 'isDediServer', $isDediServer );
+	$smarty->assign ('isDediServer', $isDediServer);
 } else {
-	define ( 'IN_INSTALL', true );
+	define ('IN_INSTALL', true);
 	include ('./include/install.php');
 	exit ();
 }
 
 // load Styles
-$styles = array ();
-$stylesDir = dir ( 'styles' );
-while ( ($entry = $stylesDir->read ()) != false ) {
-	if ($entry != "." && $entry != ".." && is_dir ( './styles/' . $entry )) {
-		if (file_exists ( './styles/' . $entry . '/style.cfg' )) {
-			$styleFile = file ( './styles/' . $entry . '/style.cfg' );
-			$keyValuePair = explode ( '=', $styleFile [0] );
-			$styles [$entry] = array (
+$styles = array();
+$stylesDir = dir('styles');
+while (($entry = $stylesDir->read()) != false) {
+	if ($entry != "." && $entry != ".." && is_dir('./styles/' . $entry)) {
+		if (file_exists('./styles/' . $entry . '/style.cfg')) {
+			$styleFile = file('./styles/' . $entry . '/style.cfg');
+			$keyValuePair = explode('=', $styleFile [0]);
+			$styles[$entry] = array(
 					'path' => $entry,
-					'name' => trim ( $keyValuePair [1] ) 
+					'name' => trim($keyValuePair[1]) 
 			);
 		}
 	}
 }
-$stylesDir->close ();
+$stylesDir->close();
 
 // Kartendetails laden
-list ( $mapName, $mapShort, $mapVersion, $mapLink, $mapCopyright, $mapSize ) = file ( "./config/$mapPath/map.txt" );
+list ($mapName, $mapShort, $mapVersion, $mapLink, $mapCopyright, $mapSize, $configBy, $configVersion) = file("./config/$mapPath/map.txt");
 
 $map = array (
 		'Name' => $mapName,
@@ -64,50 +64,52 @@ $map = array (
 		'Version' => $mapVersion,
 		'Link' => $mapLink,
 		'Copyright' => $mapCopyright,
-		'Size' => $mapSize 
+		'Size' => $mapSize,
+		'configBy' => $configBy,
+    	'configVersion' => $configVersion
 );
-$smarty->assign ( 'map', $map );
+$smarty->assign('map', $map);
 require ("./config/$mapPath/mapconfig.php");
-if (! file_exists ( "./config/$mapPath/translation/{$_SESSION ['language']}.php" )) {
+if (! file_exists("./config/$mapPath/translation/{$_SESSION ['language']}.php")) {
 	require ("./config/$mapPath/translation/$defaultLanguage.php");
 } else {
 	require ("./config/$mapPath/translation/{$_SESSION ['language']}.php");
 }
-$userLang = $_SESSION ['language'];
-$lang = array_merge ( $lang, getVehicleNames () );
+$userLang = $_SESSION['language'];
+$lang = array_merge($lang, getVehicleNames());
 
 // load installed placeables
-$placeableObjects = $placeablesLang = array ();
+$placeableObjects = $placeablesLang = array();
 
-foreach ( glob ( './config/placeables/*.xml' ) as $filename ) {
-	$placeable = simplexml_load_file ( $filename );
-	foreach ( $placeable->item as $item ) {
-		$className = strval ( $item ['filename'] );
-		$placeableObjects = array_merge ( $placeableObjects, array (
-				$className => array () 
+foreach (glob('./config/placeables/*.xml') as $filename) {
+	$placeable = simplexml_load_file($filename);
+	foreach ($placeable->item as $item) {
+		$className = strval ($item ['filename']);
+		$placeableObjects = array_merge($placeableObjects, array(
+				$className => array() 
 		) );
-		foreach ( $item->attributes () as $attribute => $value ) {
+		foreach ($item->attributes() as $attribute => $value) {
 			if ($attribute != 'filename') {
-				$placeableObjects [$className] [$attribute] = get_bool ( $value );
+				$placeableObjects[$className][$attribute] = get_bool($value);
 			}
 		}
-		foreach ( $item->children () as $childName => $childData ) {
-			if (empty ( $placeableObjects [$className] [$childName] ) || ! is_array ( $placeableObjects [$className] [$childName] )) {
-				$placeableObjects [$className] [$childName] = array ();
+		foreach ($item->children() as $childName => $childData) {
+			if (empty($placeableObjects[$className][$childName]) || ! is_array($placeableObjects[$className][$childName])) {
+				$placeableObjects[$className][$childName] = array();
 			}
-			$fillType = strval ( $childData ['name'] );
-			$placeableObjects [$className] [$childName] [$fillType] = array ();
-			foreach ( $childData->attributes () as $attribute => $value ) {
+			$fillType = strval($childData['name']);
+			$placeableObjects[$className][$childName][$fillType] = array();
+			foreach ($childData->attributes() as $attribute => $value) {
 				if ($attribute != 'name') {
-					$placeableObjects [$className] [$childName] [$fillType] [$attribute] = get_bool ( $value );
+					$placeableObjects[$className][$childName][$fillType][$attribute] = get_bool($value);
 				}
 			}
 		}
 	}
-	foreach ( $placeable->l10n->text as $text ) {
-		$key = strval ( $text ['name'] );
-		$value = strval ( $text->$userLang );
-		$placeablesLang = array_merge ( $placeablesLang, array (
+	foreach ($placeable->l10n->text as $text) {
+		$key = strval ($text ['name']);
+		$value = strval($text->$userLang);
+		$placeablesLang = array_merge($placeablesLang, array(
 				$key => $value 
 		) );
 	}
